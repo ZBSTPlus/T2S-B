3375c8c32d9400eb72567e978e9a4e3b603dfcc8
unknown <nandalurubhavitha@.com>
2024-03-27 13:42:45 +0530

    commit changes

--- project_t2s/urls.py
+++ project_t2s/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 #from .t2s.views import text_to_audio ,home
-#from django.conf import settings
-#from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/',admin.site.urls)
     #path('', home, name='home'),
     #path('text-to-audio/', text_to_audio, name='text_to_audio'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
