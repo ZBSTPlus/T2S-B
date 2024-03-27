a55671e1b0eb56d0c0de9a1d312fd42abf29b871
unknown <nandalurubhavitha@.com>
2024-03-27 12:16:47 +0530

    commit changes

--- project_t2s/settings.py
+++ project_t2s/settings.py
@@ -18,6 +18,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+#MEDIA_URL = '/images/'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -49,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.staticfiles',
 ]
 
 ROOT_URLCONF = 'project_t2s.urls'
@@ -117,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
+
+#STATICFILES_DIR =[
+#os.path.join(BASE_DIR, 'templates')
+#]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- project_t2s/urls.py
+++ project_t2s/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 #from .t2s.views import text_to_audio ,home
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,3 +11,5 @@ urlpatterns = [
     #path('', home, name='home'),
     #path('text-to-audio/', text_to_audio, name='text_to_audio'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
